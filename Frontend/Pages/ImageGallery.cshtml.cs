e2fae666a7099a022cc4a2a1a0137824f4e6b81f
Kai Roth <Kai.Roth@adn.de>
2021-10-28 14:36:58 +0200

    init

--- Frontend/Pages/ImageGallery.cshtml.cs
+++ Frontend/Pages/ImageGallery.cshtml.cs
@@ -5,15 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
 
 using Development_Praxisworkshop.Helper;
 
 namespace Development_Praxisworkshop.Pages
 {
-    //[AllowAnonymous]
-    [Authorize]
     public class GalleryModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
