83cbf579babc80dbfe3fce532fc9b3a7ec0adcc1
Kai Roth <49306768+adnkai@users.noreply.github.com>
2022-05-03 15:29:12 +0200

    Update release-annotation.sh

--- release-annotation.sh
+++ release-annotation.sh
@@ -1,7 +1,7 @@
 echo "Creating release annotation in Application Insights"
 
 APPINSIGHTS_ID=$( az resource show -g Developmentworkshop -n devworkshop --resource-type "microsoft.insights/components" --query id -o tsv)
-UUID=$(cat /proc/sys/kernel/random/uuid)
+UUID='6e8dff67-eb87-4091-9f97-128394981d76'#$(cat /proc/sys/kernel/random/uuid)
 releaseName='Frontend release'
 releaseDescription='Released frontend from Github Actions'
 triggerBy='Kai Roth'
