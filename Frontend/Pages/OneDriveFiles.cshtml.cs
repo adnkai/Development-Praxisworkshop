2f3635efb5e7e3a46421214e777b6dbe5857f8fd
Kai Roth <Kai.Roth@adn.de>
2022-07-11 09:37:53 +0200

    onedrive files anzeigen

--- Frontend/Pages/OneDriveFiles.cshtml.cs
+++ Frontend/Pages/OneDriveFiles.cshtml.cs
@@ -21,16 +21,16 @@ using Development_Praxisworkshop.Helper;
 namespace Development_Praxisworkshop.Pages
 {
     //[Authorize(Policy="MustHaveOneDrive")] // need onedrive permissions
-    [AuthorizeForScopes(Scopes = new[]{"Contacts.Read"})] // Welche scopes fordern wir auf dieser seite (ggf.) an?
+    [AuthorizeForScopes(Scopes = new[]{"files.readwrite", "Sites.Read.All"})] // Welche scopes fordern wir auf dieser seite (ggf.) an?
     public class OneDriveFilesModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
         private readonly IConfiguration _config;
         public IDriveItemsCollectionPage files;
         readonly ITokenAcquisition _tokenAcquisition;
-        
+        private string _accessToken;
         private readonly GraphServiceClient _graphServiceClient;
-
+        public IDriveItemChildrenCollectionPage _files;
         private readonly MicrosoftIdentityConsentAndConditionalAccessHandler _consentHandler;
 
         private string[] _graphScopes;
@@ -45,15 +45,30 @@ namespace Development_Praxisworkshop.Pages
             _tokenAcquisition = tokenAcquisition;
             _graphServiceClient = graphServiceClient;
             this._consentHandler = consentHandler;
-            _graphScopes = new[] {"files.readwrite"};
+            _graphScopes = new[] {"files.readwrite", "Sites.Read.All"};
         }
 
         public void OnGet()
         {
             // Scopes, welche JETZT benötigt werden.
-            string[] scopes = new string[]{"Contacts.Read","Family.Read"}; // Inkrementelle Anforderung automatisch durch bekanntgabe gaaaanz oben
-            string accessToken = _tokenAcquisition.GetAccessTokenForUserAsync(scopes).Result;
+            //string[] scopes = new string[]{"Contacts.Read","Family.Read"}; // Inkrementelle Anforderung automatisch durch bekanntgabe gaaaanz oben
+            string[] scopes = new string[]{"files.readwrite", "Sites.Read.All"}; // Inkrementelle Anforderung automatisch durch bekanntgabe gaaaanz oben
+            _accessToken = _tokenAcquisition.GetAccessTokenForUserAsync(scopes).Result;
             
+            // var graphClient = new GraphServiceClient(new DelegateAuthenticationProvider((requestMessage) => {
+            //                 // get an access token for Graph
+            //     var accessToken = _accessToken;
+            //     requestMessage
+            //         .Headers
+            //         .Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+
+            //     return Task.FromResult(0);
+            // }));
+
+            
+            //var files = graphClient.Drive.Root.Request().GetAsync().Result;
+            _files = _graphServiceClient.Drive.Root.Children.Request().GetAsync().Result;
+
         }
 
         
