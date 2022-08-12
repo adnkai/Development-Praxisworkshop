263bad1ed1ea11b7a07b68400d905317ebc1f414
ADNTobi <tobias.pitzschke@adn.de>
2022-08-12 09:49:43 +0200

    fix bug: inserts wrong partitionKey

--- Function/Table/postTodo.cs
+++ Function/Table/postTodo.cs
@@ -5,23 +5,22 @@ public static class postTodo
   public static async Task<IActionResult> Run(
       [HttpTrigger(AuthorizationLevel.Function, "post", Route = "postElement/{tableName}")] HttpRequest req,
       ILogger log, string tableName)
-  {
-    log.LogInformation("Insert new ListElementModel");
-    
+  {   
     string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-
-    var data = JsonConvert.DeserializeObject<ListElementModel>(requestBody);
     string _upn = req.Query["upn"];
 
+    var data = JsonConvert.DeserializeObject<ListElementModel>(requestBody);
+    data.PartitionKey = tableName;
+    
     ListElementModel result;
     
     try
     {
       result = await (new TableSettings(_upn)).InsertItem(tableName, data);
     }
-    catch (Exception)
+    catch (Exception e)
     {
-      return new BadRequestObjectResult(null);
+      return new BadRequestObjectResult(e.Message);
     }
 
     return new OkObjectResult(result);
@@ -36,15 +35,18 @@ public static class postTodo
     
     string _upn = req.Query["upn"];
 
+    log.LogInformation($"UPN: {_upn}");
+    log.LogInformation($"table: {tableName}");
+
     bool result;
     
     try
     {
       result = await (new TableSettings(_upn)).CreateTableForUser(tableName);
     }
-    catch (Exception)
+    catch (Exception e)
     {
-      return new BadRequestObjectResult(null);
+      return new BadRequestObjectResult(e.Message);
     }
 
     return new OkObjectResult(tableName);
